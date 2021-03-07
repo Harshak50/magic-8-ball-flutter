87f24249d53c7d4cd528b75bf05c173aaa69b3c8
Harsha <konndapuramharsha@gmail.com>
2021-03-07 13:01:27 +0530

    update

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/harshak/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/harshak/Desktop/Flutter_Kahaniya/Flutter_Udemy/Magic_Ball"
+export "FLUTTER_TARGET=/Users/harshak/Desktop/Flutter_Kahaniya/Flutter_Udemy/Magic_Ball/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/harshak/Desktop/Flutter_Kahaniya/Flutter_Udemy/Magic_Ball/.dart_tool/package_config.json"
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: askMe(),
       ),
     );
+
+// ignore: camel_case_types
+class askMe extends StatefulWidget {
+  @override
+  _askMeState createState() => _askMeState();
+}
+
+// ignore: camel_case_types
+class _askMeState extends State<askMe> {
+  int ballNumber = 1;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(4) + 1;
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
